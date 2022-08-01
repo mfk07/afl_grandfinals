f01ee2a36f4968695132359f1eb948e0c76e2576
maria@32pixels.co.uk <mfk@marias-mbp.home>
2022-08-01 16:54:48 +0100

    final update of code for review

--- index.php
+++ index.php
@@ -6,13 +6,29 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare("SELECT `Season`, `Premier`, `Runner-Up`, `Score`, `Image` FROM `grandfinals`;");
 $query->execute();
 
-$teams = $query->fetchAll();
+$finals = $query->fetchAll();
+
+//create individual premiership data blocks
+//create display function that echos out the data
+
+function getTeamData(array $finals): string {
+    $team = '';
+    foreach($finals as $final) {
+        if ($final === []) {
+            return 'missing data';
+        } else {
+            $team .= '<div class="winners-box">' .
+                        '<img src="'.$final['Image'].'" alt="Grandfinal team photo" class="winners-image">' . '<div class="match-stats"><h6 class="team-name">' . $final['Premier'] . '</h6><p>' . $final['Season'] . '</p><p>' . $final['Runner-Up'] . '</p><p>' . $final['Score'] . '</p></div></div>';
+        }
+    } 
+    return $team;
+}
+
+$winners = getTeamData($finals);
 
 
 
-// fixtures link https://www.afl.com.au/fixture?Competition=1&CompSeason=43&MatchTimezone=MY_TIME&Regions=3&ShowBettingOdds=1&GameWeeks=21&Teams=1&Venues=13#byround
-// ladder link https://www.afl.com.au/ladder?Competition=1&CompSeason=43&GameWeeks=608&ShowBettingOdds=1&Live=0
-// player stat link https://www.afl.com.au/stats/?category=Key+Stats&seasonId=43&roundId=-1&roundNumber=0&sortColumn=dreamTeamPoints&sortDirection=descending&positions=All&teams=All&benchmarking=false&dataType=totals&playerOneId=null&playerTwoId=null
+
 
 
 ?>
@@ -26,48 +42,21 @@ $teams = $query->fetchAll();
 </head>
 
 <main class="display">
-                
-				<header id="header">
-                    <nav id="menu">
-                        <ul>
-                            <li>Fixtures</li>
-                            <li>Ladder</li>
-                            <li>Player Stats</li>
-                        </ul>
-                    </nav>
-				    <h1 class="page-heading">AFL Grandfinal Winners</h1>
-                </header>
+	<header id="header">
+        <nav>
+            <ul id="menu">
+                <li><a href=https://www.afl.com.au/fixture?Competition=1&CompSeason=43&MatchTimezone=MY_TIME&Regions=3&ShowBettingOdds=1&GameWeeks=21&Teams=1&Venues=13#byround>Fixtures</a></li>
+                <li><a href=https://www.afl.com.au/ladder?Competition=1&CompSeason=43&GameWeeks=608&ShowBettingOdds=1&Live=0>Ladder</a></li>
+                <li><a href=https://www.afl.com.au/stats/?category=Key+Stats&seasonId=43&roundId=-1&roundNumber=0&sortColumn=dreamTeamPoints&sortDirection=descending&positions=All&teams=All&benchmarking=false&dataType=totals&playerOneId=null&playerTwoId=null>Player Stats</a></li>
+            </ul>
+        </nav>
+		<h1 class="page-heading">AFL Grandfinal Winners</h1>
+    </header>
 
-                <h2>2001 - 2021</h2>
-
-				<div class="portfolio-items">
-					<div class="projects">
-						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
-						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
-					</div>
-					<div class="projects">
-						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
-						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
-					</div>
-					<div class="projects">
-						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
-						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
-					</div>
-					<div class="projects">
-						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
-						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
-					</div>
-					<div class="projects">
-						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
-						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
-					</div>
-					<div class="projects">
-						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
-						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
-					</div>
-				</div>
-				
+    <h2>2001 - 2021</h2>
+    <div class="display-div">
+        <?php echo $winners;?>
+    </div>
 		</main>
 
 </html>
-
