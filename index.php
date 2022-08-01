42dfce281ec15e2617ebe897c578ab8969e71446
maria@32pixels.co.uk <mfk@marias-mbp.home>
2022-08-01 12:43:43 +0100

    updates index.php and adds gitignore, and fonts and images files

--- index.php
+++ index.php
@@ -8,9 +8,7 @@ $query->execute();
 
 $teams = $query->fetchAll();
 
-echo '<pre>';
-var_dump($teams);
-echo '</pre>';
+
 
 // fixtures link https://www.afl.com.au/fixture?Competition=1&CompSeason=43&MatchTimezone=MY_TIME&Regions=3&ShowBettingOdds=1&GameWeeks=21&Teams=1&Venues=13#byround
 // ladder link https://www.afl.com.au/ladder?Competition=1&CompSeason=43&GameWeeks=608&ShowBettingOdds=1&Live=0
@@ -19,5 +17,57 @@ echo '</pre>';
 
 ?>
 
-<h1>hello</h1>
+<html lang="en">
+<head>
+	<title>AFL Grandfinal Winners</title>
+	<link href="normalize.css" type="text/css" rel="stylesheet" />
+	<link href="style.css" type="text/css" rel="stylesheet" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<main class="display">
+                
+				<header id="header">
+                    <nav id="menu">
+                        <ul>
+                            <li>Fixtures</li>
+                            <li>Ladder</li>
+                            <li>Player Stats</li>
+                        </ul>
+                    </nav>
+				    <h1 class="page-heading">AFL Grandfinal Winners</h1>
+                </header>
+
+                <h2>2001 - 2021</h2>
+
+				<div class="portfolio-items">
+					<div class="projects">
+						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
+						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
+					</div>
+					<div class="projects">
+						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
+						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
+					</div>
+					<div class="projects">
+						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
+						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
+					</div>
+					<div class="projects">
+						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
+						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
+					</div>
+					<div class="projects">
+						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
+						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
+					</div>
+					<div class="projects">
+						<img src="portfolio-placeholder.jpg" alt="Portfolio Placeholder" class="portfolio-placeholder">
+						<h6 class="project-description">Website created July 2022 as part of the iO Academy Full Stack Track.</h6>
+					</div>
+				</div>
+				
+		</main>
+
+</html>
 
