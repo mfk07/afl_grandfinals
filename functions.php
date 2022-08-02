590a70c2618d216ef05f1d7e66297764d021db58
maria@32pixels.co.uk <mfk@marias-mbp.home>
2022-08-02 13:45:27 +0100

    final php, css and testing for review

--- functions.php
+++ functions.php
@@ -1,11 +1,22 @@
 <?php
 
+/**
+ * Access database
+ *
+ * @return void
+ */
 function getDb() {
     $db = new PDO('mysql:host=db; dbname=afl', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
 }
 
+/**
+ * Access database params
+ *
+ * @param [type] $db
+ * @return void
+ */
 function getData($db) {
     $query = $db->prepare("SELECT `Season`, `Premier`, `Runner-Up`, `Score`, `Image` FROM `grandfinals`;");
     $query->execute();
@@ -13,11 +24,17 @@ function getData($db) {
     return $query->fetchAll();
 }
 
+/**
+ * Access arrays from database
+ *
+ * @param array $finals
+ * @return string
+ */
 function getTeamData(array $finals): string {
     $team = '';
     foreach($finals as $final) {
         if ($final === []) {
-            return 'missing data';
+            return 'Got data?';
         } else {
             $team .= '<div class="winners-box">' .
                         '<img src="'.$final['Image'].'" alt="Grandfinal team photo" class="winners-image">' . '<div class="match-stats"><h6 class="team-name">' . $final['Premier'] . '</h6><p>' . $final['Season'] . '</p><p>' . $final['Runner-Up'] . '</p><p>' . $final['Score'] . '</p></div></div>';
--- test/functions.php
+++ test/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../index.php';
+require_once '../functions.php';
 
 use PHPUnit\Framework\TestCase;
 
@@ -8,37 +8,36 @@ class Functions extends TestCase {
     public function testSuccessGetTeamData()
     {
         //expected result of the test
-        $expected = [];
+        $expected = '<div class="winners-box">' .
+        '<img src="a.jpeg" alt="Grandfinal team photo" class="winners-image">' . '<div class="match-stats"><h6 class="team-name">' . 'Port Adelaide Power' . '</h6><p>' . 1999 . '</p><p>' . 'Adelaide Crows' . '</p><p>' . '10.6 (66) d. 5.3 (33)' . '</p></div></div>';
         //input for the test to get the result
-        $testInput1 = 3;
-        $testInput2 = 2;
+        $testInput1 = [['Image' => 'a.jpeg', 'Season' => 1999, 'Premier' => 'Port Adelaide Power', 'Runner-Up' => 'Adelaide Crows', 'Score' => '10.6 (66) d. 5.3 (33)']];
         //run the real function with the input
-        $case = sum($testInput1, $testInput2);
+        $case = getTeamData($testInput1);
         //compare the expected result with the actual result
         $this->assertEquals($expected, $case);
     }
 
-    public function testFailureMultiplyByTwo()
+    public function testFailureGetTeamData()
     {
         //expected result of the test
-        $expected = 0;
+        $expected = 'Got data?';
         //input for the test to get the result
-        $testInput1 = -3;
-        $testInput2 = 2;
+        $testInput1 = [[]];
         //run the real function with the input
-        $case = sum($testInput1, $testInput2);
+        $case = getTeamData($testInput1);
         //compare the expected result with the actual result
         $this->assertEquals($expected, $case);
     }
 
-    public function testMalformedMultiplyByTwo()
+    public function testMalformedGetTeamData()
     {
         //input for the test to get the result
-        $testInput1 = [1,2,3,4];
-        $testInput2 = [4,5,6,7];
+        $testInput1 = 2;
         // tell phpunit it should expect an error to be thrown
         $this->expectException(TypeError::class);
         //run the real function with the input
-        $case = sum($testInput1, $testInput2);
+        $case = getTeamData($testInput1);
         
     }
+}
