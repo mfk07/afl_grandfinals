44213254c8ba0719b490f421b2dcd9fa9a9cb8a7
mfk <maria@32pixels.co.uk>
2022-08-04 15:23:22 +0100

    code review changes made

--- functions.php
+++ functions.php
@@ -52,8 +52,7 @@ function sendData($db, $getSeason, $getPremier, $getRunnersUp, $getScore): void
 function getTeamData(array $finals): string 
 {
     $team = '';
-    foreach($finals as $final) 
-        {
+    foreach($finals as $final) {
         $team .= '<div class="winners-box">' . '<img src="'.$final['Image'].'" alt="Grandfinal team photo" class="winners-image">' . '<div class="match-stats"><h6 class="team-name">' . $final['Premier'] . '</h6><p>' . $final['Season'] . '</p><p>' . $final['Runner-Up'] . '</p><p>' . $final['Score'] . '</p></div>' . '<form action="delete.php" method="POST"><input type="submit" value="Delete"/><input class="hidden" name="dataID" value="' . $final['id'] . '"></form>' . '<form class="edit" action="edit.php" method="POST"><input type="submit" value="Edit"/><input class="hidden" name="dataID" value="' . $final['id'] . '"></form></div>';
         }    
     return $team;
@@ -84,7 +83,7 @@ function removeData($db, $id)
 
 function getSingleData($db, $id): array
 {
-    $query = $db->prepare("SELECT `Season`, `Premier`, `Runner-Up`, `Score`, `Image` FROM `grandfinals` WHERE `id` = $id;");
+    $query = $db->prepare("SELECT `id`, `Season`, `Premier`, `Runner-Up`, `Score`, `Image` FROM `grandfinals` WHERE `id` = $id;");
     $query->execute();
 
     return $query->fetchAll();
@@ -94,15 +93,16 @@ function prefillEditData(array $singleData): string
 {
     foreach($singleData as $index) 
         {
-        $data = '<form action="edit.php" method="POST">' .  '<label for="editSeason">What year was the final played?</label><br>' . '<input type="text" id="season" name="editSeason"/><br><br>' . '<label for="editPremier">Who won the flag?</label><br>' . '<input type="text" id="premier" name="editPremier"/><br><br>' . '<label for="editRunnersUp">Who did they play?</label><br>' . '<input type="text" id="runnersUp" name="editRunnersUp"/><br><br>' . '<label for="editScore">What was the score? (GG.PP (TOTAL) d. GG.PP (TOTAL))</label><br>' . '<input type="text" id="score" name="editScore"/><br><br>' . '<input class="hidden" name="dataID" value="' . $final['id'] . '">' . '<input type="submit"/>' . '</form>';
+        $data = '<div class="winners-box"><form action="edit.php" method="POST"><label for="editSeason">What year was the final played?</label><br><input type="text" id="season" name="editSeason" value="'.$index['Season'].'"/><br><br>' . '<label for="editPremier">Who won the flag?</label><br>' . '<input type="text" id="premier" name="editPremier" value="'.$index['Premier'].'"/><br><br>' . '<label for="editRunnersUp">Who did they play?</label><br>' . '<input type="text" id="runnersUp" name="editRunnersUp" value="'.$index['Runner-Up'].'"/><br><br>' . '<label for="editScore">What was the score? (GG.PP (TOTAL) d. GG.PP (TOTAL))</label><br>' . '<input type="text" id="score" name="editScore" value="'.$index['Score'].'"/><br><br>' . '<input class="hidden" name="dataID" value="' . $index['id'] . '">' . '<input type="submit"/>' . '</form></div>';
         
         }    
     return $data;
 }
 
+
 function editData($db, $editSeason, $editPremier, $editRunnersUp, $editScore, $id) 
 {
-    $query = $db->prepare("UPDATE `grandfinals` SET `Premier` =   WHERE `id`= ?;");
+    $query = $db->prepare("UPDATE `grandfinals` SET `Season` =  $editSeason, `Premier` =  $editPremier, `Runner-Up` =  $editRunnersUp, `Score` = $editScore WHERE `id`= ?;");
     $query->execute([$id]);
     return true;
 }
