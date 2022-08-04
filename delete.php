6ec1ba946a993d1f9c3138ee376dcf98fe3bf3e3
maria@32pixels.co.uk <mfk@marias-mbp.home>
2022-08-04 10:34:00 +0100

    completed delete button and functionality

--- delete.php
+++ delete.php
@@ -4,27 +4,7 @@ session_start();
 require_once 'functions.php';
 
 $id = targetID();
-
-echo '<pre>';
-var_dump($id);
-echo '</pre>';
-
-/**
- * Removes selected ID from the front-end
- *
- * @param [type] $db
- * @param [type] $getID
- * @return void
- */
-function removeData($db, $id) {
-    $query = $db->prepare("UPDATE `grandfinals` SET `delete` = `1` WHERE `id`= '$id';");
-    $query->execute();
-
-    return $query->fetchAll();
-}
-
-echo '<pre>';
-var_dump("UPDATE `grandfinals` SET `delete` = `1` WHERE `id`= ':id';");
-echo '</pre>';
+$db = getDb();
+removeData($db, $id);
 
 ?>
